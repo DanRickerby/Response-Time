1f3de4a57232f96217538c2642e478abe52dbb1c
Daniel Rickerby <DanRickerby@users.noreply.github.com>
2019-10-18 08:58:08 +1300

    Airport changes

--- Reaction Test/scripts.js
+++ Reaction Test/scripts.js
@@ -1,4 +1,23 @@
+//Develop into the random time interval
+function wait(ms) {
+	var wd = new Date();
+	var wd2 = null;
+	do { wd2 = new Date(); }
+	while(wd2-wd < ms);
+}
+
+function time(div) {
+	var td = new Date();
+	var td2 = null;
+}
+
 function playTone() {
-        var tone = new Audio("Tone.wav");
-        tone.play();
-    }
+    var tone = new Audio("Tone.wav");
+    tone.play();
+}
+
+function visual(div) {
+	div.style.background = "black";
+	wait(2000);
+	div.style.background = "green";
+}
